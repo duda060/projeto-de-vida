4a6541f4fc13c6db6e1e0f10e2fd123db415347d
duda060 <maria.farias.taborda@escola.pr.gov.br>
2025-10-10 14:26:28 -0300

    .

---
+++ main.js
@@ -0,0 +1,8 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".abaconteudo");
+for (lei i = 0; i < botoes.length;i++) {
+    botoes[1].onclik = function () {
+        for (let j = 0;j < botoes.length;j++){
+            
+        }
+    }
