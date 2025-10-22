03dcef947984b1933d9da3950498ac2d3e3d4abb
duda060 <maria.farias.taborda@escola.pr.gov.br>
2025-10-22 15:04:32 -0300

    .

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const textos = document.querySelectorAll(".abaconteudo");
 for (lei i = 0; i < botoes.length;i++) {
     botoes[1].onclik = function () {
         for (let j = 0;j < botoes.length;j++){
-            
+            botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");            
         }
     }
+    const tempoObjetivo1 = new Date("2023-10-05T00:00:00");
+    const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
+    const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+    const tempoObjetivo4 + new Date("2024-02-01T00:00:00");
+    const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+    
